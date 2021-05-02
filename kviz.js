d70fc65f9137cf4f69e8f4ca9f4fc367631b882d
libuse.poustkova <libuse.poustkova@skoda.cz>
2021-05-02 20:41:24 +0200

    funkcni vsechny stranky

--- kviz.js
+++ kviz.js
@@ -15,15 +15,21 @@ const poleOtazek = [{
     otazka: 'Pro úspěšné absolvování kurzu je potřeba ...',
     odpoved:['Umět JavaScript', 'Chodit po kurzu do hospody'],
     spravne: 'Umět JavaScript'
-}]
+},
+{
+    foto: 'obrazky/snehurka.jpg',
+    otazka: 'Kdo je na světě nejkrásnější?',
+    odpoved:['já', 'Sněhurka', 'Zeman', ' BB'],
+    spravne: 'Sněhurka'
+}
+]
 const kviz = document.createElement('div');
 
-let pocatek = document.querySelector('h1');
+let pocatek = document.querySelector('body');
 let i = 0;
 let pocetOtazek = poleOtazek.length;
 let pole_odpovedi = [];
-
-
+let ok = 0;
 
 
 function priNacteni() {
@@ -45,7 +51,7 @@ function nacteni_otazky(i) {
     obsah.id ='moznosti';
 
 
-    const otazka = document.createElement('h1');
+    const otazka = document.createElement('h2');
     otazka.id = 'otazka';
     otazka.textContent = poleOtazek[i].otazka;
 
@@ -62,9 +68,10 @@ function nacteni_otazky(i) {
     fotoOtazka.src = poleOtazek[i].foto;
 
 
-    kviz.appendChild(poradi);
-    poradi.appendChild(otazka);
-    otazka.appendChild(obsah);
+    kviz.appendChild(moznosti);
+    moznosti.appendChild(poradi);
+    moznosti.appendChild(otazka);
+    moznosti.appendChild(obsah);
     obsah.appendChild(fotoOtazka);
     obsah.appendChild(odpovedi);
 
@@ -74,9 +81,9 @@ function nacteni_otazky(i) {
         let od_part = document.createElement('li');
         od_part.className ='li';
         od_part.innerHTML = poleOtazek[i].odpoved[j];
-        od_part.onclick = function (event) {console.log(event.target.innerHTML);
+        od_part.onclick = function (event) {
                                             console.log(pole_odpovedi);
-                                        kviz.removeChild(poradi);
+                                        kviz.removeChild(moznosti)
                                         pole_odpovedi.push(event.target.innerHTML);
                                         i=i+1
                                         console.log(i)
@@ -96,25 +103,53 @@ function vyhodnot_otazku(){
 
 function udelejHTMLodpovedi(ele){
     let od_part = document.createElement('li');
-    od_part.className ='odpoved';
+    //od_part.className ='odpovedi';
     od_part.innerHTML = ele;
 
     }
 
 function nacteni_vyhodnoceni(){
-     for (let j = 0; j < pocetOtazek; j=j+1 ) {
-        const otazka = document.createElement('h2');
-        otazka.id = 'otazka';
-        otazka.textContent = j + '.' +poleOtazek[j].otazka;
-        const tvoje_odpoved = document.createElement('div');
-        tvoje_odpoved.textContent = pole_odpovedi[j]
-
 
      const vysledek = document.createElement('div');
-     vysledek.id ='vysledek';
-     vysledek.textContent = 'Správně ' + i + pocetOtazek + 'otázek. Úspěšnost' + '%.'
-     kviz.appendChild(otazka);
-     otazka.appendChild(tvoje_odpoved)
- }
+     vysledek.className = 'vysledek';
 
+     const vyhodnoceni =  document.createElement('h2');
+     vyhodnoceni.textContent = 'tvoje hodnocení';
+     pocatek.removeChild(kviz);
+
+     pocatek.appendChild(vysledek);
+     vysledek.appendChild(vyhodnoceni);
+
+
+     for (let j = 0; j < pocetOtazek; j=j+1 ) {
+        const otazka = document.createElement('h3');
+        otazka.id = 'vysledek_odpovedi'
+        otazka.textContent = (j+1) + '. '+ poleOtazek[j].otazka;
+
+        const tvoje_odpoved = document.createElement('div');
+        tvoje_odpoved.textContent = 'Tvoje odpověď: '+ pole_odpovedi[j];
+        tvoje_odpoved.className = 'vysledek_odpovedi'
+
+        const spravna_odpoved = document.createElement('div');
+        spravna_odpoved.className = 'vysledek_odpovedi'
+
+        vysledek.appendChild(otazka);
+        vysledek.appendChild(tvoje_odpoved);
+        console.log(pole_odpovedi[j]);
+        console.log(poleOtazek[j].spravne);
+
+        if (pole_odpovedi[j] == poleOtazek[j].spravne) {
+        spravna_odpoved.textContent = 'To je SPRÁVNĚ.';
+
+        ok = ok + 1}
+        else {
+        spravna_odpoved.textContent = 'Správná odpověď: ' + poleOtazek[j].spravne;
+        }
+        vysledek.appendChild(spravna_odpoved);
+     }
+
+ const zaver = document.createElement('h2');
+ let uspesnost = Math.round(ok/pocetOtazek*100)
+ zaver.textContent = 'Správně ' + ok + ' ze ' + pocetOtazek + ' otázek. Úspěšnost ' + uspesnost + ' %.'
+ vysledek.appendChild(zaver)
 }
